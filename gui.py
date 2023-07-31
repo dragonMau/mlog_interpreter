9b7fd39fe08d0793f305eb43669d8e9c581bc9ee
dragonau <mau.drogon@gmail.com>
2023-08-01 00:33:46 +0300

    1.4 (removed gui.py, renamed gui_mp.py > gui.py)

--- gui.py
+++ gui.py
@@ -1,25 +1,18 @@
+import multiprocessing as mp
 import gui_.structure
 import gui_.editor
 import gui_.console
 import run
-
-
-events_data = []
-def gen_event(self, t, data=0):
-    n = len(events_data)
-    events_data.append(data)
-    self.tk.call(('event', 'generate', '.', str(t), '-x', str(n)))
     
 class Executor(run.Executor):
-    nodes: dict
-    def __init__(self, source="", code="") -> None:
-        super().__init__(source, code)
-    
+    out: dict
     def _print(self, out):
         out_type, out_cont, out_dest = out
         if out_type == "text":
-            if out_dest in self.nodes.keys():
-                self.nodes[out_dest].put(out_cont)
+            if out_dest in self.out.keys():
+                self.out[out_dest] = self.out[out_dest] + [out_cont]
+            else:
+                self.out[out_dest] = [out_cont]
 
 
 class Application:
@@ -27,56 +20,64 @@ class Application:
         self.root = gui_.structure.Window()
         self.editor = gui_.editor.Editor(self.root.container1_1)
         self.console = gui_.console.Console(self.root.container1_2)
-        self.executor = None
         
-        self.root.bind('<<test>>', self.test1)
+        self.manager = mp.Manager()
+        self.executor = mp.Value("H", 0)
+        self.executor_mem = self.manager.dict()
+        self.executor_out = self.manager.dict()
+        self.executor_lock = mp.Lock()
+        self.p: mp.Process = None
+        
         self.root.bind('<<run_code>>', self.run_code)
 
-    def loop_executor(self, data):
-        data["prev_mem"] = self.executor.mem.mem.copy()
-        self.executor.step()
-        data["iterations"] += 1
-        if any((
-            data["iterations"] > 10000,
-            data["prev_mem"] == self.executor.mem.mem
-        )):
-            self.stop_executor()
-        else:
-            self.root.after(1, self.loop_executor, data)
+    @staticmethod
+    def loop_executor(lock, code, mem, alive, out):
+        executor = Executor(code=code)
+        executor.out = out
+        mem.clear()
+        for k, v in executor.mem.mem.items():
+            mem[k] = v
+        executor.mem.mem = mem
+        iterations = 0
+        alive.value = 1
+        while iterations < 10000:
+            prev_mem = executor.mem.mem.copy()
+            lock.acquire()
+            executor.step()
+            lock.release()
+            iterations += 1
+            if prev_mem == executor.mem.mem:
+                break
+        alive.value = 2
 
     def stop_executor(self):
-        self.executor = None
-
-    def run_executor(self):
-        if not self.executor: return
-        self.loop_executor({
-            "iterations": 0,
-            "prev_mem": {}
-        })
-    
+        self.executor.value = 0
+        
     def run_code(self, *_):
-        if self.executor: return
-        self.executor = Executor(code=self.editor.code.text.get("1.0", "end"))
-        self.executor.nodes = {
+        if self.executor.value: return
+        code = self.editor.code.text.get("1.0", "end")
+        self.executor_nodes = {
             tab.terminal._name: tab.terminal.out for tab in self.console.tabs_frame.winfo_children()
             if "out" in dir(tab.terminal)
         }
-        self.run_executor()
-    
-    def test(self):
-        gen_event(self.root, "<<test>>", ("console0", "test"))
-        
-    def test1(self, e):
-        data = events_data.pop(e.x)
-        dest, text = data
-        for tab in self.console.tabs_frame.winfo_children():
-            if tab.terminal._name == dest:
-                tab.terminal.out.put(text)
+        self.executor_out.clear()
+        self.executor.value = 0
+        self.p = mp.Process(target=self.loop_executor, args=(self.executor_lock, code, self.executor_mem, self.executor, self.executor_out))
+        self.p.start()
                 
     def every_frame(self):
-        if self.executor:
-            pointer = self.executor.mem.mem.get("@counter", 0)
+        self.executor_lock.acquire()
+        if self.executor.value:
+            pointer = self.executor_mem.get("@counter", 0)
             self.editor.code.highlighted_lines = {pointer: (">", "#0e0")}
+            for k, v in self.executor_out.items():
+                if k in self.executor_nodes.keys():
+                    while v:
+                        self.executor_nodes[k].put(v.pop(0))
+            self.executor_out.clear()
+            if self.executor.value == 2:
+                self.stop_executor()
+        self.executor_lock.release()
         self.root.after(30, self.every_frame)
     
     def mainloop(self):
@@ -85,6 +86,5 @@ class Application:
 
 if __name__=="__main__":
     app = Application()
-    # app.root.after(5000, app.test)
 
     app.mainloop()
